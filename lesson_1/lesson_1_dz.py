9c954ab9093b869c79a2533e67d46f59cf92b44e
KonievaOlha <grc.olga2201@gmail.com>
2023-11-01 23:57:06 +0200

    Add DZ for lesson_1

---
+++ lesson_1/lesson_1_dz.py
@@ -0,0 +1,33 @@
+# Завдання №1
+number1 = 16
+number2 = 9
+# Варіант №1
+suma = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+fraction = number1 / number2
+
+print(suma)
+print(difference)
+print(product)
+print(fraction)
+
+# Варіант №2
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+# Завдання №2
+number_1 = int(input("Введіть перше число: "))
+number_2 = int(input("Введіть друге число: "))
+result_2 = number_1 % number_2
+print(result_2)
+#print(number_1 % number_2)
+
+# Завдання №3
+number_3 = int(input("Введіть перше число: "))
+number_4 = int(input("Введіть друге число: "))
+result_3 = number_3 // number_4
+print(result_3)
+#print(number_3 // number_4)
