4cbbfe106c4b4e5b12831ce5483e3b470b155a59
KonievaOlha <grc.olga2201@gmail.com>
2023-11-02 21:08:19 +0200

    Add DZ for lesson_1

--- lesson_1/lesson_1_dz.py
+++ lesson_1/lesson_1_dz.py
@@ -1,33 +1,84 @@
-# Завдання №1
-number1 = 16
-number2 = 9
-# Варіант №1
-suma = number1 + number2
-difference = number1 - number2
-product = number1 * number2
-fraction = number1 / number2
+# # Завдання №1
+# number1 = 16
+# number2 = 9
+# # Варіант №1
+# suma = number1 + number2
+# difference = number1 - number2
+# product = number1 * number2
+# fraction = number1 / number2
+#
+# print(suma)
+# print(difference)
+# print(product)
+# print(fraction)
+#
+# # Варіант №2
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+#
+# # Завдання №2
+# number_1 = int(input("Введіть перше число: "))
+# number_2 = int(input("Введіть друге число: "))
+# result_2 = number_1 % number_2
+# print(result_2)
+# #print(number_1 % number_2)
+#
+# # Завдання №3
+# number_3 = int(input("Введіть перше число: "))
+# number_4 = int(input("Введіть друге число: "))
+# result_3 = number_3 // number_4
+# print(result_3)
+# #print(number_3 // number_4)
+#
+# # Завдання №4
+# text_1 = int(input('Введіть ваш рік народження'))
+# print(type(text_1))
+# print(text_1)
+#
+# Завдання №5
+# num_1 = input('введіть число 1: ')
+# num_2 = input('введіть число 2: ')
+# print(num_1 + num_2)
+# print(int(num_1) + int(num_2))
 
-print(suma)
-print(difference)
-print(product)
-print(fraction)
+# Завдання №6
+# name = input("Введіть Ваше ім'я: ")
+# birthday = int(input("Введіть Ваш рік народження: "))
+# year_now = int(input("Введіть, який зараз рік: "))
+# age = year_now - birthday
+# print("Ваше ім'я", name, "і Вам", age, "років")
 
-# Варіант №2
-print(number1 + number2)
-print(number1 - number2)
-print(number1 * number2)
-print(number1 / number2)
+# Завдання №7
 
-# Завдання №2
-number_1 = int(input("Введіть перше число: "))
-number_2 = int(input("Введіть друге число: "))
-result_2 = number_1 % number_2
-print(result_2)
-#print(number_1 % number_2)
+# Завдання №8
 
-# Завдання №3
-number_3 = int(input("Введіть перше число: "))
-number_4 = int(input("Введіть друге число: "))
-result_3 = number_3 // number_4
-print(result_3)
-#print(number_3 // number_4)
+# ---Завдання №9 с^2 = a^2 + b^2
+# side_a = int(input("Введіть довжину катета а: "))
+# side_b = int(input("Введіть довжину катета b: "))
+# side_c = (side_a**2) + (side_b**2)
+# print(side_c)
+
+# ??? Завдання №10
+# n_child = int(input("Введіть кількість школярів: "))
+# k_apple = int(input("Введіть кількість яблук: "))
+# child = k_apple // n_child
+# basket = k_apple % n_child
+# print("Школяреві дістанеться яблук", child)
+# print("Яблук в кошику залишиться", basket)
+
+# Завдання №11
+pencil = int(input(""))
+pen = int(input(""))
+marker = int(input(""))
+
+
+
+
+
+
+
+
+
+
