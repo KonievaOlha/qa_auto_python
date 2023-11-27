c477a72198458597efec83fe67227dcfc1f9d0ae
KonievaOlha <grc.olga2201@gmail.com>
2023-11-27 12:57:10 +0200

    add lesson_3.py

--- lesson_3/lesson_3_DZ.py
+++ lesson_3/lesson_3_DZ.py
@@ -32,11 +32,18 @@ else:
 #якщо додавати декілька продуктів
 add_product = input("Введіть через пробіл продукти, які бажаєте додати: ")
 add_prod_list = add_product.split()
-list_of_products = list_of_products + add_prod_list
+# list_of_products = list_of_products + add_prod_list
+list_of_products.append(add_prod_list)
 print(f"Ваш новий список: {list_of_products}! Гарного дня!")
 
 
 # #якщо додавати лише один продукт
 # add_product = input("Введіть продукт, який бажаєте додати: ")
 # list_of_products.append(add_product)
+# print(f"Ваш новий список: {list_of_products}! Гарного дня!")
+
+# #якщо додавати список в список
+# add_product = input("Введіть через пробіл продукти, які бажаєте додати: ")
+# add_prod_list = add_product.split()
+# list_of_products.append(add_prod_list)
 # print(f"Ваш новий список: {list_of_products}! Гарного дня!")
