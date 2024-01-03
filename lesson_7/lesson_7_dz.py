cd62f6a0710a734766eb616305c0b7afb98f09a8
KonievaOlha <grc.olga2201@gmail.com>
2024-01-03 20:05:44 +0200

    add lesson_7_dz.py

--- lesson_7/lesson_7_dz.py
+++ lesson_7/lesson_7_dz.py
@@ -2,7 +2,8 @@
 #
 # Створіть два файли
 # в 1-му напишіть такі функції:
-# 1) сортування списка по зростанню числа, від меншого до більшого. Функція приймає список з чисел і повертає відсортований список.
+# 1) сортування списка по зростанню числа, від меншого до більшого. Функція приймає список з чисел і повертає
+# відсортований список.
 # 2) сортування списка на спад, від більшого до меншого. Функція приймає список з чисел і повертає відсортований список.
 # 3) сортування за кількістю букв у слові. Функція приймає список з слів і повертає відсортований список.
 #
@@ -12,39 +13,34 @@
 
 list_growth = [100, 13, 79, 1, 25]
 list_words = ["яблоко", "апельсин", "вікно", "мило", "гелікоптер"]
+list_growth_for_assert = []
 
 
-def sort_list_growth(list_growth:list) -> list:
-    list_growth.sort()
-    print(list_growth)
-   # return list_growth
+def sort_list_growth(list_growth_d: list) -> list:
+    list_growth_d.sort()
+    print(list_growth_d)
+    return list_growth_d
 
 
+def sort_list_desc(list_growth_d: list) -> list:
+    list_growth_d.sort(reverse=True)
+    print(list_growth_d)
+    return list_growth_d
 
 
-
-def sort_list_desc(list_growth:list) -> list:
-    list_growth.sort(reverse = True)
-    print(list_growth)
-    return list_growth
-
+def sort_list_words(list_words_d: list) -> list:
+    list_words_d.sort(key=len)
+    print(list_words_d)
+    return list_words_d
 
 
+def sort_list_growth_for_assert(list_growth_for_assert_d: list) -> list:
+    list_growth_for_assert_d.sort()
+    print(list_growth_for_assert_d)
+    return list_growth_for_assert_d
 
 
-def sort_list_words(list_words:list) -> list:
-    list_words.sort(key=len)
-    print(list_words)
-    return list_words
-
-# sort_list_growth([100, 13, 79, 1, 25])
-# sort_list_desc([100, 13, 79, 1, 25])
-# sort_list_words(["яблоко", "апельсин", "вікно", "мило", "гелікоптер"])
-
-# sorted
-# list_1 = [34, 456, 11, 12, -100, 77, 100, 35, 100.0, 100]
-# list_2 = ['груша', 'грушаa', 'банан',  'яблуко', 'диня', "слива", "апельсин"]
-# print(sorted(list_1, reverse=True))
-# print(sorted(list_1, key=lambda x: x%2))
-# print(sorted(list_2, key=len))
-# print(sorted(list_2, key=lambda a: a[-1]))
+# sort_list_growth(list_growth)
+# sort_list_desc(list_growth)
+# sort_list_words(list_words)
+# sort_list_growth_for_assert(list_growth_for_assert)
--- lesson_7/test_lesson_7_dz.py
+++ lesson_7/test_lesson_7_dz.py
@@ -1,8 +1,10 @@
 from lesson_7_dz import sort_list_growth
 from lesson_7_dz import sort_list_desc
 from lesson_7_dz import sort_list_words
+from lesson_7_dz import sort_list_growth_for_assert
 from lesson_7_dz import list_growth
 from lesson_7_dz import list_words
+from lesson_7_dz import list_growth_for_assert
 
 
 def test_sort_list_growth():
@@ -15,3 +17,11 @@ def test_sort_list_desc():
 
 def test_sort_list_words():
     sort_list_words(list_words)
+
+
+# def test_sort_list_words_with_assert():
+#     assert "мило" not in sort_list_words(list_words), "Лист містить слово 'мило'"
+#
+#
+# def test_sort_list_growth_with_assert():
+#     assert len(sort_list_growth_for_assert(list_growth_for_assert)) != 0, "List is empty"
