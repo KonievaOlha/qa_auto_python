25acc6f506f636c4e1fd43e144f253acfdeb19c7
KonievaOlha <grc.olga2201@gmail.com>
2024-02-02 13:37:29 +0200

    add lesson_10_dz.py

---
+++ lesson_9_10/dz_test_param_10.py
@@ -0,0 +1,33 @@
+import pytest
+from dz_lesson_9_10 import add_numbers
+
+
+# 1
+def test_add_1():
+    assert add_numbers(1, 2, 3) == 6
+
+
+def test_add_2():
+    assert add_numbers(3, 5, 7) == 15
+
+
+def test_add_3():
+    assert add_numbers(10, 15, 11) == 36
+
+
+# 2
+@pytest.mark.parametrize("var_1, var_2, var_3, result", [
+    pytest.param(3, 5, 7, 15, id="simple"),
+    pytest.param(-5, 6, 2, 3, id="with negative"),
+    pytest.param(-2, -6, 3, -5, id="one positive")])
+def test_add_set(var_1, var_2, var_3, result):
+    assert add_numbers(var_1, var_2, var_3) == result
+
+
+# 3
+list_add_test = [(3, 5, 7, 15), (-5, 6, 2, 3), (-2, -6, 3, -5)]
+
+
+@pytest.mark.parametrize("var_1, var_2, var_3, result", list_add_test)
+def test_add_set_2(var_1, var_2, var_3, result):
+    assert add_numbers(var_1, var_2, var_3) == result
